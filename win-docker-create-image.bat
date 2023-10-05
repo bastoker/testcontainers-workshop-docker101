ffdab8dd0d91476a1292887153e23d8f7d25a1b7
Bas Stoker <bas@bas.st>
2023-10-05 14:15:13 +0200

    .

--- win-docker-create-image.bat
+++ win-docker-create-image.bat
@@ -1,10 +1,5 @@
 @echo off
 
-REM Get the current Git branch name
-for /f "delims=" %%i in ('git symbolic-ref --short HEAD 2^>nul') do (
-    set "branch=%%i"
-)
-
 REM Build the Docker image with the tag docker-101
-docker build -tag docker-101:%tag% .
+docker build -tag docker-101:main .
 
